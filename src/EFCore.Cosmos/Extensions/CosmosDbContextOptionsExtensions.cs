974083c83bd862f535b00686cdecf0d970f89043
MysticBoy <mysticboy@live.com>
2022-02-14 13:25:48 +0800

    使用cosmos 作为模板

---
+++ src/EFCore.Cosmos/Extensions/CosmosDbContextOptionsExtensions.cs
@@ -0,0 +1,145 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore.Cosmos.Infrastructure;
+using Microsoft.EntityFrameworkCore.Cosmos.Infrastructure.Internal;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.EntityFrameworkCore.Cosmos.Extensions;
+
+/// <summary>
+///     Cosmos-specific extension methods for <see cref="DbContextOptionsBuilder" />.
+/// </summary>
+/// <remarks>
+///     See <see href="https://aka.ms/efcore-docs-dbcontext-options">Using DbContextOptions</see>, and
+///     <see href="https://aka.ms/efcore-docs-cosmos">Accessing Azure Cosmos DB with EF Core</see> for more information and examples.
+/// </remarks>
+public static class CosmosDbContextOptionsExtensions
+{
+    /// <summary>
+    ///     Configures the context to connect to an Azure Cosmos database.
+    /// </summary>
+    /// <remarks>
+    ///     See <see href="https://aka.ms/efcore-docs-dbcontext-options">Using DbContextOptions</see>, and
+    ///     <see href="https://aka.ms/efcore-docs-cosmos">Accessing Azure Cosmos DB with EF Core</see> for more information and examples.
+    /// </remarks>
+    /// <typeparam name="TContext">The type of context to be configured.</typeparam>
+    /// <param name="optionsBuilder">The builder being used to configure the context.</param>
+    /// <param name="accountEndpoint">The account end-point to connect to.</param>
+    /// <param name="accountKey">The account key.</param>
+    /// <param name="databaseName">The database name.</param>
+    /// <param name="cosmosOptionsAction">An optional action to allow additional Cosmos-specific configuration.</param>
+    /// <returns>The options builder so that further configuration can be chained.</returns>
+    public static DbContextOptionsBuilder<TContext> UseCosmos<TContext>(
+        this DbContextOptionsBuilder<TContext> optionsBuilder,
+        string accountEndpoint,
+        string accountKey,
+        string databaseName,
+        Action<CosmosDbContextOptionsBuilder>? cosmosOptionsAction = null)
+        where TContext : DbContext
+        => (DbContextOptionsBuilder<TContext>)((DbContextOptionsBuilder)optionsBuilder).UseCosmos(
+            accountEndpoint,
+            accountKey,
+            databaseName,
+            cosmosOptionsAction);
+
+    /// <summary>
+    ///     Configures the context to connect to an Azure Cosmos database.
+    /// </summary>
+    /// <remarks>
+    ///     See <see href="https://aka.ms/efcore-docs-dbcontext-options">Using DbContextOptions</see>, and
+    ///     <see href="https://aka.ms/efcore-docs-cosmos">Accessing Azure Cosmos DB with EF Core</see> for more information and examples.
+    /// </remarks>
+    /// <param name="optionsBuilder">The builder being used to configure the context.</param>
+    /// <param name="accountEndpoint">The account end-point to connect to.</param>
+    /// <param name="accountKey">The account key.</param>
+    /// <param name="databaseName">The database name.</param>
+    /// <param name="cosmosOptionsAction">An optional action to allow additional Cosmos-specific configuration.</param>
+    /// <returns>The options builder so that further configuration can be chained.</returns>
+    public static DbContextOptionsBuilder UseCosmos(
+        this DbContextOptionsBuilder optionsBuilder,
+        string accountEndpoint,
+        string accountKey,
+        string databaseName,
+        Action<CosmosDbContextOptionsBuilder>? cosmosOptionsAction = null)
+    {
+        Check.NotNull(optionsBuilder, nameof(optionsBuilder));
+        Check.NotNull(accountEndpoint, nameof(accountEndpoint));
+        Check.NotEmpty(accountKey, nameof(accountKey));
+        Check.NotEmpty(databaseName, nameof(databaseName));
+
+        var extension = optionsBuilder.Options.FindExtension<CosmosOptionsExtension>()
+            ?? new CosmosOptionsExtension();
+
+        extension = extension
+            .WithAccountEndpoint(accountEndpoint)
+            .WithAccountKey(accountKey)
+            .WithDatabaseName(databaseName);
+
+        ((IDbContextOptionsBuilderInfrastructure)optionsBuilder).AddOrUpdateExtension(extension);
+
+        cosmosOptionsAction?.Invoke(new CosmosDbContextOptionsBuilder(optionsBuilder));
+
+        return optionsBuilder;
+    }
+
+    /// <summary>
+    ///     Configures the context to connect to an Azure Cosmos database.
+    /// </summary>
+    /// <remarks>
+    ///     See <see href="https://aka.ms/efcore-docs-dbcontext-options">Using DbContextOptions</see>, and
+    ///     <see href="https://aka.ms/efcore-docs-cosmos">Accessing Azure Cosmos DB with EF Core</see> for more information and examples.
+    /// </remarks>
+    /// <typeparam name="TContext">The type of context to be configured.</typeparam>
+    /// <param name="optionsBuilder">The builder being used to configure the context.</param>
+    /// <param name="connectionString">The connection string of the database to connect to.</param>
+    /// <param name="databaseName">The database name.</param>
+    /// <param name="cosmosOptionsAction">An optional action to allow additional Cosmos-specific configuration.</param>
+    /// <returns>The options builder so that further configuration can be chained.</returns>
+    public static DbContextOptionsBuilder<TContext> UseCosmos<TContext>(
+        this DbContextOptionsBuilder<TContext> optionsBuilder,
+        string connectionString,
+        string databaseName,
+        Action<CosmosDbContextOptionsBuilder>? cosmosOptionsAction = null)
+        where TContext : DbContext
+        => (DbContextOptionsBuilder<TContext>)((DbContextOptionsBuilder)optionsBuilder).UseCosmos(
+            connectionString,
+            databaseName,
+            cosmosOptionsAction);
+
+    /// <summary>
+    ///     Configures the context to connect to an Azure Cosmos database.
+    /// </summary>
+    /// <remarks>
+    ///     See <see href="https://aka.ms/efcore-docs-dbcontext-options">Using DbContextOptions</see>, and
+    ///     <see href="https://aka.ms/efcore-docs-cosmos">Accessing Azure Cosmos DB with EF Core</see> for more information and examples.
+    /// </remarks>
+    /// <param name="optionsBuilder">The builder being used to configure the context.</param>
+    /// <param name="connectionString">The connection string of the database to connect to.</param>
+    /// <param name="databaseName">The database name.</param>
+    /// <param name="cosmosOptionsAction">An optional action to allow additional Cosmos-specific configuration.</param>
+    /// <returns>The options builder so that further configuration can be chained.</returns>
+    public static DbContextOptionsBuilder UseCosmos(
+        this DbContextOptionsBuilder optionsBuilder,
+        string connectionString,
+        string databaseName,
+        Action<CosmosDbContextOptionsBuilder>? cosmosOptionsAction = null)
+    {
+        Check.NotNull(optionsBuilder, nameof(optionsBuilder));
+        Check.NotNull(connectionString, nameof(connectionString));
+        Check.NotNull(databaseName, nameof(databaseName));
+
+        var extension = optionsBuilder.Options.FindExtension<CosmosOptionsExtension>()
+            ?? new CosmosOptionsExtension();
+
+        extension = extension
+            .WithConnectionString(connectionString)
+            .WithDatabaseName(databaseName);
+
+        ((IDbContextOptionsBuilderInfrastructure)optionsBuilder).AddOrUpdateExtension(extension);
+
+        cosmosOptionsAction?.Invoke(new CosmosDbContextOptionsBuilder(optionsBuilder));
+
+        return optionsBuilder;
+    }
+}
---
+++ src/EFCore.Cosmos/Metadata/Internal/CosmosPropertyExtensions.cs
@@ -0,0 +1,34 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore.Cosmos.Extensions;
+
+namespace Microsoft.EntityFrameworkCore.Cosmos.Metadata.Internal;
+
+/// <summary>
+///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+///     any release. You should only use it directly in your code with extreme caution and knowing that
+///     doing so can result in application failures when updating to a new Entity Framework Core release.
+/// </summary>
+public static class CosmosPropertyExtensions
+{
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public static bool IsOrdinalKeyProperty(this IReadOnlyProperty property)
+    {
+        Check.DebugAssert(
+            property.DeclaringEntityType.IsOwned(), $"Expected {property.DeclaringEntityType.DisplayName()} to be owned.");
+        Check.DebugAssert(property.GetJsonPropertyName().Length == 0, $"Expected {property.Name} to be non-persisted.");
+
+        return property.FindContainingPrimaryKey() is IReadOnlyKey key
+            && key.Properties.Count > 1
+            && !property.IsForeignKey()
+            && property.ClrType == typeof(int)
+            && property.ValueGenerated == ValueGenerated.OnAdd;
+    }
+}
